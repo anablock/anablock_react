2efb2aa24ffac39014e2b790eaa53ea0fdaa8a24
Vuk Dukic <vukdukic@Vuks-MacBook-Pro.local>
2019-12-16 21:28:38 -0800

    removing css files from src control

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from './cmp/Button'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <button>PRESS ME</button>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
