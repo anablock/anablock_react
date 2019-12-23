06fa67cc5010e6af02222e7a00f12aa02c9d7e5f
Vuk Dukic <vukdukic@Vuks-MacBook-Pro.local>
2019-12-22 18:37:22 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container, Navbar, Jumbotron, Button, Row } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar inverse fixedTop>
+          <Container>
+                <a href="/">React App</a>
+          </Container>
+        </Navbar>
+        <Jumbotron>
+          
+        </Jumbotron>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thank you for visiting <code>Anablock.com</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://anablock.com"
           target="_blank"
           rel="noopener noreferrer"
         >
         </a>
       </header>
+      <div>Hello</div>
     </div>
   );
 }
